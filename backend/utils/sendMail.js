5399232076366a3a1b5460381865aea3d5e5c1ae
pratikgiri10 <pratikgiri2320@gmail.com>
2025-08-03 19:57:19 +0545

    fix: fix credentials

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -6,16 +6,16 @@ export const sendMail = async (message) => {
     
     const transporter = nodemailer.createTransport({
     service: "Gmail",
-    host: 'smtp.gmail.coom',
-    port: 465,
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
     secure: true, // true for 465, false for other ports
     auth: {
-        user: 'collab0310@gmail.com',
-        pass: 'jqry sxss klut tphp',
+        user: process.env.SMTP_EMAIL,
+        pass: process.env.SMTP_PASS,
     },
 });
 await transporter.sendMail({
-    from: 'DigitalDokan<collab0310@gmail.com>',
+    from: `DigitalDokan<${process.env.SMTP_HOST}>`,
     to: 'pratikgiri2320@gmail.com',
     subject: 'Password Reset Token',
     text: message,
