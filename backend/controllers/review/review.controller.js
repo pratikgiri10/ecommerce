a8dfe99fd58efe8402b7496119e4067b229de52f
pratikgiri10 <pratikgiri2320@gmail.com>
2025-08-08 19:39:11 +0545

    ui: ui fixes

--- backend/controllers/review/review.controller.js
+++ backend/controllers/review/review.controller.js
@@ -1,4 +1,5 @@
 import Review from "../../models/review.model.js";
+import { ApiError } from "../../utils/ApiError.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 
@@ -18,8 +19,22 @@ export const postReview = asyncHandler(async(req,res) => {
     
 })
 export const updateReview = asyncHandler(async(req,res) => {
+    const review = await Review.findById(req.params.id)
+    if(!review)
+        throw new ApiError(404, 'review not found')
+    if(review.user !== req.user._id)
+        throw new ApiError(401, 'you are not authorized')
 
+    const updatedReview = await Review.findByIdAndUpdate(req.params.id, req.body, {new : true})
+    res.status(200).json(new ApiResponse(200, updatedReview, 'review updated successfully'))
 })
 export const deleteReview = asyncHandler(async(req,res) => {
-    
+    const review = await Review.findById(req.params.id)
+    if(!review)
+        throw new ApiError(404, 'review not found')
+    if(review.user !== req.user._id)
+        throw new ApiError(401, 'you are not authorized')
+
+    await Review.deleteOne({_id: req.params.id})
+    res.status(200).json(new ApiResponse(200, null, 'review deleted successsfully'))
 })
